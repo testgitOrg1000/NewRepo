def4921585710ea7b195079ae9b2af01010ba822
Vijay Seth <vijay.seth12@rediffmail.com>
2024-03-01 14:34:09 -0600

    THis is the test change for clone repo

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Raining"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Raining", "Summer"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
